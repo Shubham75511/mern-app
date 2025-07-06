db669f10dcb1bc060716c4b5846d60aa74d5cd97
Shubham75511 <shubhamkharche1410@gmail.com>
2025-07-06 19:01:43 +0530

    install react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
